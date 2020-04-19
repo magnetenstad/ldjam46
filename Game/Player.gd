960956cecd0b8d6804e59da9888e070d3851862b
hansmariusoveras <hansmariusoveras@gmail.com>
2020-04-20 01:05:39 +0200

    level design, set fire all directions, sound, game over

--- Game/Player.gd
+++ Game/Player.gd
@@ -6,22 +6,28 @@ var grav = 300
 var jump = 150
 var spd_max = 80
 var burns = []
-
+onready var gameover_message= $"/root/Main/World/Player/Camera2D/Control"
+onready var audio = $"Audio"
 var bonfire
+var last_checkpoint_position = Vector2(0, 0)
 
-var flammable = [0, 1, 2]
+var flammable = [0, 1]
 
 var health = 1
 
 func image_set_flip(flip):
 	get_node("Sprite").set_flip_h(flip)
 	
+	
 func _on_ready():
-	 pass
+	pass
 
 func _process(delta):
 	if(bonfire):
 		health = 1
+		if (last_checkpoint_position - position).length() > 4:
+			audio.play()
+		last_checkpoint_position = position
 	var light_factor = (max(min((float(150) - get_position().y), float(50)), float(0)) / float(50))
 	$"../CanvasModulate".color = Color(0.0 + 1 * light_factor, 0.0 + 1 * light_factor, 0.0 + 1 * light_factor)
 	
@@ -45,6 +51,7 @@ func _physics_process(delta):
 		image_set_flip(true)
 	
 	var tilemap = $"/root/Main/World/TileMap"
+	var firemap = $"/root/Main/World/FireTileMap"
 	var pos = tilemap.world_to_map(position)
 	var cell = tilemap.get_cell(pos.x, pos.y)
 	
@@ -54,16 +61,34 @@ func _physics_process(delta):
 		velocity.y = -jump/4
 	elif is_on_floor() and Input.is_action_just_pressed("ui_up"):
 		velocity.y = -jump
+	# respawn to checkpoint
 	
-	pos.y += 1
+	if Input.is_key_pressed(KEY_R):
+		position = last_checkpoint_position
+		jump = 150
+		spd_max = 80
+		health = 1
+		audio.play()
 	
 	# set fire
 	
-	cell = tilemap.get_cell(pos.x, pos.y)
-	if health > 0 and cell in flammable and Input.is_action_pressed("ui_down"):
-		tilemap.set_cell(pos.x, pos.y, 8)
-		burns.append([floor(rand_range(2, 60)), pos])
-		health = 1
+	var direction = -(int(get_node("Sprite").is_flipped_h())*2-1) # 1: left, 0: right
+								
+	if health > 0 and Input.is_action_pressed("ui_down"):
+		var firepos = Vector2(-42069, -42069)
+		if tilemap.get_cell(pos.x + direction, pos.y) in flammable:
+			firepos = Vector2(pos.x+direction, pos.y)
+		elif tilemap.get_cell(pos.x, pos.y-1) in flammable:
+			firepos = Vector2(pos.x, pos.y-1)
+		elif tilemap.get_cell(pos.x, pos.y+1) in flammable:
+			firepos = Vector2(pos.x, pos.y+1)
+			
+		if firepos != Vector2(-42069, -42069):
+			tilemap.set_cell(firepos.x, firepos.y, 8)
+			firemap.set_cell(firepos.x, firepos.y, 8)
+			burns.append([floor(rand_range(2, 60)), firepos])
+			health = 1
+			audio.play()
 		
 	# fire spreading
 	
@@ -73,6 +98,7 @@ func _physics_process(delta):
 		burn[0] -= 1
 		
 		if burn[0] <= 0:
+			firemap.set_cell(pos.x, pos.y, -1)
 			tilemap.set_cell(pos.x, pos.y, -1)
 			burns.remove(i)
 			break
@@ -83,7 +109,9 @@ func _physics_process(delta):
 				cell = tilemap.get_cell(other.x, other.y)
 				if burn[0] == 1 and cell in flammable:
 					tilemap.set_cell(other.x, other.y, 8)
+					firemap.set_cell(other.x, other.y, 8)
 					burns.append([floor(rand_range(2, 60)), other])
+					audio.play()
 
 	var world = $"/root/Main/World"
 	
@@ -98,6 +126,12 @@ func _physics_process(delta):
 			if collision and collision.collider == enemy:
 				enemy.queue_free()
 			
-	health = max(0, health - 0.05 * delta)	
+	health = max(0, health - 0.05 * delta)
+	if health == 0:
+		gameover_message.show()
+		spd_max = 0
+		jump = 0
+	else:
+		gameover_message.hide()
 
 	
--- Game/Wood.gd
+++ Game/Wood.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+onready var audio = $"/root/Main/World/Player/Audio"
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -17,6 +17,7 @@ func _process(delta):
 	var player = $"/root/Main/World/Player"
 	
 	if position.distance_to(player.position) < 16:
+		audio.play()
 		player.health = 1
 		queue_free()
 		
