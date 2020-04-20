653d960a4b88db1e979d4a62dc8371e110984b20
Eivind <eivindseriksen@gmail.com>
2020-04-20 20:28:12 +0200

    Fix enemy; Add water source and water droplets

---
+++ Game/WaterSource.gd
@@ -0,0 +1,16 @@
+extends Sprite
+
+const WATER_DROPLET = preload("res://WaterDroplet.tscn")
+var timer = 0
+
+func _ready():
+	pass
+
+func _process(delta):
+	timer += delta
+	if(timer > 3):
+		timer = 0
+		var water_droplet = WATER_DROPLET.instance()
+		add_child_below_node(self, water_droplet)
+		print("test")
+		#water_droplet.set_position(get_position())
